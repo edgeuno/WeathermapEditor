894b7a5e35bb706a70bfd452ceef394d855ddf94
Javier Alejandro Ruiz <javier.ruiz@edgeuno.com>
2021-09-22 14:56:06 -0500

    Changes to the port retrieval

--- editor/jsm.php
+++ editor/jsm.php
@@ -162,7 +162,7 @@ switch ($action) {
       foreach ($devices as $device) {
         if (!is_null($device->ports)) {
           foreach ($device->ports as $port) {
-            $rra_path = js_escape('./') . $device->hostname . '/port-id';
+            $rra_path = './' . $device->hostname . '/port-id';
             $fullpath = $rra_path . $port->port_id . '.rrd:INOCTETS:OUTOCTETS';;
             $graph_url = $base_url . 'graph.php?height=100&width=512&id=' . $port->port_id . '&type=port_bits&legend=no';
             $info_url = $base_url . 'graphs/type=port_bits/id=' . $port->port_id . '/';
